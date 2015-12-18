ce084a1f3fffa68a94e9d71da6a3be0867bce7ca
yuzhangiot <yuzhangiot@gmail.com>
2015-12-18 17:29:18 +0800

    mac left

--- app/controllers/gauges_controller.rb
+++ app/controllers/gauges_controller.rb
@@ -1,2 +1,15 @@
 class GaugesController < ApplicationController
+	def index
+		set_channels_menu
+		@channel = Channel.find(params[:channel_id])
+		@channel_id = params[:channel_id]
+		@domain = domain
+		#set default value of model gauge
+		#here
+		check_permissions(@channel)		
+	end
+
+	def show
+		
+	end
 end
